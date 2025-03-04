75d22163e9494885e13a886e378e59dd856627a3
Artem Tarasenko <shabashab.04@gmail.com>
2025-03-04 09:44:12 +0200

    fix(di): eslint errors

--- packages/di/.eslintrc.cjs
+++ packages/di/.eslintrc.cjs
@@ -3,8 +3,12 @@ const path = require('path')
 module.exports = {
   parserOptions: {
     project: [
-      path.resolve(__dirname, 'tsconfig.json'), 
-      path.resolve(__dirname, 'tsconfig.test.json')
-    ]
+      path.resolve(__dirname, 'tsconfig.json'),
+      path.resolve(__dirname, 'tsconfig.test.json'),
+      path.resolve(__dirname, 'tsconfig.config.json'),
+    ],
+  },
+  rules: {
+    'no-redeclare': 'off',
   },
 }
